d46803a24daa0a374bd2bf7497be9efa2a98041e
Leadenbrain <Leadenbrain@users.noreply.github.com>
2019-03-24 02:35:48 -0400

    Track platform directory

---
+++ EnGL/src/Platform/Windows/WinInput.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "EnGL/Input.h"
+
+namespace EnGL {
+	
+	class WinInput : public Input
+	{
+	protected:
+		virtual bool IsKeyPressedImpl(int keycode) override;
+
+		virtual bool IsMouseButtonPressedImpl(int button) override;
+		virtual std::pair<float, float> GetMousePositionImpl() override;
+		virtual float GetMouseXImpl() override;
+		virtual float GetMouseYImpl() override;
+	};
+
+}
